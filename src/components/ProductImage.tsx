336bad1850497bfa1b58bf0291f87fdf8ab1814d
umamaheswari29122008-del <umamaheswari29122008@gmail.com>
2026-07-17 07:45:28 +0530

    Update product image to HD PNG

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function ProductImage({ className, style }: Props) {
   return (
     <img
-      src="/uploads/PHOTO-2026-07-16-20-33-03-removebg-preview.png"
+      src="/uploads/PHOTO-2026-07-16-20-33-03-removebg-preview_imgupscaler.ai_General_2K-Photoroom.png"
       alt="Dove Ice Gel Pack"
       className={className}
       style={style}
